f7c4525522580a35d216a8ee86c374cb116d014f
sillycat <jpwolfgamer@gmail.com>
2024-06-03 19:13:15 +0100

    commit

--- app/api/[storeId]/products/route.ts
+++ app/api/[storeId]/products/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 import { auth } from "@clerk/nextjs";
 
 import prismadb from "@/lib/prismadb";
@@ -132,31 +132,4 @@ export async function GET(
         console.log('[PRODUCTS_GET]', error);
         return new NextResponse("Internal Error", { status: 500 });
     }
-}
-
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-export async function SEARCH(req: NextRequest) {
-  const { searchParams } = new URL(req.url);
-  const q = searchParams.get('q');
-
-  if (!q || typeof q !== 'string') {
-    return NextResponse.json({ message: 'Invalid query' }, { status: 400 });
-  }
-
-  try {
-    const response = await fetch(`${API_URL}/products/search?q=${q}`);
-    
-    if (!response.ok) {
-      throw new Error('Error fetching products');
-    }
-
-    const products = await response.json();
-    return NextResponse.json(products, { status: 200 });
-  } catch (error) {
-    if (error instanceof Error) {
-      return NextResponse.json({ message: error.message }, { status: 500 });
-    }
-    return NextResponse.json({ message: 'Unknown error' }, { status: 500 });
-  }
 }
