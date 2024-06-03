3a2939bcfdc15b60e754b28dfb432cd66a060c3e
sillycat <jpwolfgamer@gmail.com>
2024-06-03 23:13:19 +0100

    hopefully works

--- app/api/[storeId]/products/search/route.ts
+++ app/api/[storeId]/products/search/route.ts
@@ -1,29 +1,38 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 import prismadb from "@/lib/prismadb";
-import { auth } from "@clerk/nextjs";
-const API_URL = process.env.FRONTEND_STORE_URL;
 
-export async function GET(req: NextRequest) {
-    const { searchParams } = new URL(req.url);
-    const q = searchParams.get('q');
-  
-    if (!q || typeof q !== 'string') {
-      return NextResponse.json({ message: 'Invalid query' }, { status: 400 });
-    }
-  
+export async function GET(req: Request, { params }: { params: { storeId: string } }) {
     try {
-      const response = await fetch(`${API_URL}/products/search?q=${q}`);
-      
-      if (!response.ok) {
-        throw new Error('Error fetching products');
-      }
-  
-      const products = await response.json();
-      return NextResponse.json(products, { status: 200 });
+        const { searchParams } = new URL(req.url);
+        const searchQuery = searchParams.get("q") || undefined;  // Ensure searchQuery is either a string or undefined
+
+        if (!params.storeId) {
+            return new NextResponse("Store Id is required", { status: 400 });
+        }
+
+        const products = await prismadb.product.findMany({
+            where: {
+                storeId: params.storeId,
+                name: searchQuery ? {
+                    contains: searchQuery,
+                    mode: 'insensitive', // Optional: makes the search case-insensitive
+                } : undefined,
+                isArchived: false,
+            },
+            include: {
+                images: true,
+                category: true,
+                brand: true,
+                color: true,
+            },
+            orderBy: {
+                createdAt: 'desc',
+            },
+        });
+
+        return NextResponse.json(products);
     } catch (error) {
-      if (error instanceof Error) {
-        return NextResponse.json({ message: error.message }, { status: 500 });
-      }
-      return NextResponse.json({ message: 'Unknown error' }, { status: 500 });
+        console.log('[PRODUCTS_SEARCH_GET]', error);
+        return new NextResponse("Internal Error", { status: 500 });
     }
-  }
+}
